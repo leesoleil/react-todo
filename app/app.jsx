51ba044cde64d00e1a0c1506d848de6f2e061785
Jay Lee <JLMac15@Jayui-MacBook-Pro.local>
2016-05-30 23:24:18 +0900

    Refactor TodoSearch to work with redux store

--- app/app.jsx
+++ app/app.jsx
@@ -11,10 +11,10 @@ var store = require('configureStore').configure();
 store.subscribe(()=>{
   console.log('New state', store.getState());
 });
-
-store.dispatch(actions.addTodo('Clean the yard'));
-store.dispatch(actions.setSearchText('yard'));
-store.dispatch(actions.toggleShowCompleted());
+//
+// store.dispatch(actions.addTodo('Clean the yard'));
+// store.dispatch(actions.setSearchText('yard'));
+// store.dispatch(actions.toggleShowCompleted());
 
 // Load foundation
 $(document).foundation();
